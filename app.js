80a225530e4865884077a1f8eb0633b9a9bd9ac7
MaltaWebDev <MaltaWebDev@gmail.com>
2024-12-29 15:56:29 +0100

    fix: forgot to wrap form elements in the form el itself

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 const bookLibrary = [];
 const container = document.querySelector(".container");
+const submitBtn = document.getElementById("submit");
 function Book(title, author, pages, haveRead) {
     return {
         title,
@@ -21,7 +22,6 @@ addBookToLibrary("River God", "Wilbur Smith", 500, true);
 addBookToLibrary("The Count of Monte Cristo", "Alexandre Dumas", 1000, false);
 addBookToLibrary("The Da Vinci Code", "Dan Brown", 600, false);
 addBookToLibrary("The Lord of the Rings", "J.R.R Tolkien", 1500, true);
-console.log(bookLibrary);
 bookLibrary.forEach((book) => {
     const card = document.createElement("div");
     card.classList.add("card");
@@ -36,4 +36,8 @@ bookLibrary.forEach((book) => {
     pages.innerHTML = `Pages: ${book.pages.toString()}`;
     card.appendChild(pages);
     container === null || container === void 0 ? void 0 : container.appendChild(card);
+});
+submitBtn === null || submitBtn === void 0 ? void 0 : submitBtn.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("form submitted!");
 });
--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 const bookLibrary: Book[] = [];
 const container = document.querySelector(".container");
+const submitBtn = document.getElementById("submit");
 
 interface Book {
     title: string;
@@ -44,8 +45,6 @@ addBookToLibrary("The Count of Monte Cristo", "Alexandre Dumas", 1000, false);
 addBookToLibrary("The Da Vinci Code", "Dan Brown", 600, false);
 addBookToLibrary("The Lord of the Rings", "J.R.R Tolkien", 1500, true);
 
-console.log(bookLibrary);
-
 bookLibrary.forEach((book) => {
     const card = document.createElement("div");
     card.classList.add("card");
@@ -60,4 +59,9 @@ bookLibrary.forEach((book) => {
     pages.innerHTML = `Pages: ${book.pages.toString()}`;
     card.appendChild(pages);
     container?.appendChild(card);
+});
+
+submitBtn?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("form submitted!");
 });
